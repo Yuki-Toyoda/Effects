55945207273f3cc293eb012b913180b33e609e2e
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2022-11-05 17:23:48 +0900

    回復エフェクト完成

--- EffectTemplate/main.cpp
+++ EffectTemplate/main.cpp
@@ -56,6 +56,7 @@ struct HealEffect {
 	float theta;
 	float elapseFrame;
 	float time;
+	float PlusTime;
 	float easeTime;
 	unsigned int currentAlpha;
 
@@ -85,65 +86,141 @@ struct Player {
 	関数宣言ここから
 *********************************/
 
-/******** エフェクト更新処理 **********/
-void HealEffectUpdate(HealEffect& healEffect, Player& player) {
+/******** サークルエフェクト更新処理 **********/
+void HealCircleEffectUpdate(HealEffect& healCircleEffect, Player& player) {
 
-	if (healEffect.init == true) {
+	/******** 初期化 **********/
+	if (healCircleEffect.init == true) {
 
 		//エフェクトの位置、速度、サイズ初期化
-		healEffect.position = { player.position.x, player.position.y };
-		healEffect.velocity = { My::RandomF(5.0f, 7.0f, 1), My::RandomF(5.0f, 7.0f, 1) };
-		healEffect.size = { 1, 1 };
+		healCircleEffect.position = { player.position.x, player.position.y };
+		healCircleEffect.velocity = { My::RandomF(5.0f, 7.0f, 1), My::RandomF(5.0f, 7.0f, 1) };
+		healCircleEffect.size = { 1, 1 };
 
-		healEffect.time = 0.0f;
+		healCircleEffect.time = 0.0f;
+		healCircleEffect.PlusTime = 0.06f;
 
-		healEffect.currentAlpha = 0xFF;
-
-		//エフェクトが向かう方向をランダムにする
-		healEffect.theta = My::Random(0, 180);
-		healEffect.theta = healEffect.theta * (M_PI / 180.0f);
+		healCircleEffect.currentAlpha = 0xFF;
 
 		//エフェクト表示
-		healEffect.isEnd = false;
+		healCircleEffect.isEnd = false;
 
 		//初期化フラグfalse
-		healEffect.init = false;
+		healCircleEffect.init = false;
 
 	}
 
-	if (healEffect.elapseFrame >= 100 || healEffect.time < 1.00f) {
+	/******** 終了処理 **********/
+	if (healCircleEffect.elapseFrame >= 100 || healCircleEffect.time > 0.55f) {
 
-		if (healEffect.currentAlpha > 0xFF) {
+		//エフェクトを徐々に透明に
+		healCircleEffect.currentAlpha -= 0x0A;
+
+		//透明度が0になったら
+		if (healCircleEffect.currentAlpha > 0xFF) {
 			//エフェクト消去
-			healEffect.isEnd = true;
+			healCircleEffect.isEnd = true;
 			//経過フレーム初期化
-			healEffect.elapseFrame = 0.0f;
+			healCircleEffect.elapseFrame = 0.0f;
+		}
+
+	}
+
+	/******** 更新処理 **********/
+	if (healCircleEffect.isEnd == false) {
+
+		//エフェクト位置をプレイヤーに固定
+		healCircleEffect.position = { player.position.x, player.position.y };
+
+		//円のイージング処理
+		healCircleEffect.time += healCircleEffect.PlusTime;
+		if (healCircleEffect.PlusTime >= 0.0f) {
+			healCircleEffect.PlusTime -= 0.001f;
 		}
 		else {
-			healEffect.currentAlpha -= 0x06;
+			healCircleEffect.PlusTime = 0.001f;
 		}
+		healCircleEffect.easeTime = 1.0f - powf(1.0f - healCircleEffect.time, 3.0f);
 
-	}
-
-	if (healEffect.isEnd == false) {
-
-		healEffect.position = { player.position.x, player.position.y };
-
-		if (healEffect.time < 1.35f) {
-			healEffect.time += 0.035f;
-		}
-		healEffect.easeTime = 1.0f - powf(1.0f - healEffect.time, 3.0f);
-
-		healEffect.size.x = (1.0 - healEffect.easeTime) * 1 + healEffect.easeTime * 75;
-		healEffect.size.y = (1.0 - healEffect.easeTime) * 1 + healEffect.easeTime * 75;
+		//円のサイズを変える
+		healCircleEffect.size.x = (1.0 - healCircleEffect.easeTime) * 1 + healCircleEffect.easeTime * 75;
+		healCircleEffect.size.y = (1.0 - healCircleEffect.easeTime) * 1 + healCircleEffect.easeTime * 75;
 
 		//経過フレーム加算
-		healEffect.elapseFrame += 1.0f;
+		healCircleEffect.elapseFrame += 1.0f;
 
 	}
 
 }
 
+/******** 粒子エフェクト更新処理 **********/
+void HealParticleEffectUpdate(HealEffect& healParticleEffect, Player& player) {
+
+	/******** 初期化 **********/
+	if (healParticleEffect.init == true) {
+
+		//エフェクトの位置、速度、サイズ初期化
+		healParticleEffect.position = { My::RandomF(player.position.x - 50.0f, player.position.x + 50.0f, 1), My::RandomF(player.position.y - 35.0f, player.position.y + 45.0f, 1) };
+		healParticleEffect.startPosition = { My::RandomF(15.0f, 25.0f, 1), healParticleEffect.position.y };
+		healParticleEffect.velocity = { My::RandomF(5.0f, 7.0f, 1), My::RandomF(5.0f, 7.0f, 1) };
+		healParticleEffect.size = { 1, 1 };
+
+		healParticleEffect.time = My::RandomF(0.0f, 0.1f, 1);
+		healParticleEffect.PlusTime = 0.03f;
+
+		healParticleEffect.currentAlpha = 0xFF;
+
+		//エフェクト表示
+		healParticleEffect.isEnd = false;
+
+		//初期化フラグfalse
+		healParticleEffect.init = false;
+
+	}
+
+	/******** 終了処理 **********/
+	if (healParticleEffect.elapseFrame >= 100 || healParticleEffect.time > 0.25f) {
+
+		//エフェクトを徐々に透明に
+		healParticleEffect.currentAlpha -= 0x06;
+
+		//透明度が0になったら
+		if (healParticleEffect.currentAlpha > 0xFF) {
+			//エフェクト消去
+			healParticleEffect.isEnd = true;
+			//経過フレーム初期化
+			healParticleEffect.elapseFrame = 0.0f;
+		}
+
+	}
+
+	/******** 更新処理 **********/
+	if (healParticleEffect.isEnd == false) {
+
+		//粒子エフェクトのイージング処理
+		healParticleEffect.time += healParticleEffect.PlusTime;
+		if (healParticleEffect.PlusTime >= 0.0f) {
+			healParticleEffect.PlusTime -= 0.00001f;
+		}
+		else {
+			healParticleEffect.PlusTime = 0.001f;
+		}
+		healParticleEffect.easeTime = 1.0f - powf(1.0f - healParticleEffect.time, 3.0f);
+
+		//粒子エフェクトのサイズ変更
+		healParticleEffect.size.x = (1.0 - healParticleEffect.easeTime) * 0.1f + healParticleEffect.easeTime * 5;
+		healParticleEffect.size.y = (1.0 - healParticleEffect.easeTime) * 0.1f + healParticleEffect.easeTime * 5;
+
+		//粒子エフェクトを徐々に上に
+		healParticleEffect.easeTime = healParticleEffect.time * healParticleEffect.time;
+		healParticleEffect.position.y = (1.0 - healParticleEffect.easeTime) * healParticleEffect.startPosition.y + healParticleEffect.easeTime * (healParticleEffect.startPosition.y - healParticleEffect.startPosition.x);
+
+		//経過フレーム加算
+		healParticleEffect.elapseFrame += 1.0f;
+
+	}
+
+}
 /*********************************
 	関数宣言ここまで
 *********************************/
@@ -169,16 +246,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	/******** エフェクト関係 **********/
 	//表示可能エフェクト数
-	const int maxEffects = 1;
+	const int maxCircleEffects = 1; //サークル
+	const int maxParticleEffects = 10; //粒子
 
-	//エフェクト
-	HealEffect healEffect[maxEffects];
-	for (int i = 0; i < maxEffects; i++) {
-		healEffect[i] = {
+	//サークルエフェクト
+	HealEffect healCircleEffect[maxCircleEffects];
+	for (int i = 0; i < maxCircleEffects; i++) {
+		healCircleEffect[i] = {
 			{0.0f, 0.0f},
 			{0.0f, 0.0f},
 			{0.0f, 0.0f},
 			{1.0f, 1.0f},
+			0.0f,
+			0.0f,
+			0.0f,
+			0.0f,
+			0.0f,
+			0.0f,
+			0xFF,
+			false,
+			true
+		};
+	}
+
+	//粒子エフェクト
+	HealEffect healParticleEffect[maxParticleEffects];
+	for (int i = 0; i < maxParticleEffects; i++) {
+		healParticleEffect[i] = {
+			{0.0f, 0.0f},
+			{0.0f, 0.0f},
+			{0.0f, 0.0f},
+			{1.0f, 1.0f},
+			0.0f,
 			0.0f,
 			0.0f,
 			0.0f,
@@ -217,13 +316,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		*********************************/
 
 		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
-			for (int i = 0; i < maxEffects; i++) {
-				healEffect[i].init = true;
+			for (int i = 0; i < maxCircleEffects; i++) {
+				healCircleEffect[i].init = true;
+			}
+			for (int i = 0; i < maxParticleEffects; i++) {
+				healParticleEffect[i].init = true;
 			}
 		}
 
-		for (int i = 0; i < maxEffects; i++) {
-			HealEffectUpdate(healEffect[i], player);
+		for (int i = 0; i < maxCircleEffects; i++) {
+			HealCircleEffectUpdate(healCircleEffect[i], player);
+		}
+		for (int i = 0; i < maxParticleEffects; i++) {
+			HealParticleEffectUpdate(healParticleEffect[i], player);
 		}
 
 		/******** 移動 **********/
@@ -246,34 +351,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/*********************************
 			描画処理ここから
 		*********************************/
-		/******** エフェクト描画 **********/
-		for (int i = 0; i < maxEffects; i++) {
-			if (!healEffect[i].isEnd) {
-				Novice::DrawQuad(
-					healEffect[i].position.x - healEffect[i].size.x,
-					healEffect[i].position.y + healEffect[i].size.y,
-
-					healEffect[i].position.x + healEffect[i].size.x,
-					healEffect[i].position.y + healEffect[i].size.y,
-
-					healEffect[i].position.x - healEffect[i].size.x,
-					healEffect[i].position.y - healEffect[i].size.y,
-
-					healEffect[i].position.x + healEffect[i].size.x,
-					healEffect[i].position.y - healEffect[i].size.y,
-
-					0, 0,
-					32, 32,
-
-					wireCircleTexture,
-					0xFFFFFFFF00 + healEffect[i].currentAlpha
-				);
-			}
-		}
 
 		Novice::DrawQuad(
 			player.position.x - player.radius,
-			player.position.y - player.radius, 
+			player.position.y - player.radius,
 
 			player.position.x + player.radius,
 			player.position.y - player.radius,
@@ -291,7 +372,60 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			RED
 		);
 
-		Novice::ScreenPrintf(0, 10, "time %4.2f", healEffect[0].time);
+		/******** エフェクト描画 **********/
+		for (int i = 0; i < maxCircleEffects; i++) {
+			if (!healCircleEffect[i].isEnd) {
+				Novice::DrawQuad(
+					healCircleEffect[i].position.x - healCircleEffect[i].size.x,
+					healCircleEffect[i].position.y + healCircleEffect[i].size.y,
+
+					healCircleEffect[i].position.x + healCircleEffect[i].size.x,
+					healCircleEffect[i].position.y + healCircleEffect[i].size.y,
+
+					healCircleEffect[i].position.x - healCircleEffect[i].size.x,
+					healCircleEffect[i].position.y - healCircleEffect[i].size.y,
+
+					healCircleEffect[i].position.x + healCircleEffect[i].size.x,
+					healCircleEffect[i].position.y - healCircleEffect[i].size.y,
+
+					0, 0,
+					32, 32,
+
+					wireCircleTexture,
+					0xFFFFFFFF00 + healCircleEffect[i].currentAlpha
+				);
+			}
+		}
+
+		/******** エフェクト描画 **********/
+		for (int i = 0; i < maxParticleEffects; i++) {
+			if (!healParticleEffect[i].isEnd) {
+				Novice::DrawQuad(
+					healParticleEffect[i].position.x - healParticleEffect[i].size.x,
+					healParticleEffect[i].position.y + healParticleEffect[i].size.y,
+
+					healParticleEffect[i].position.x + healParticleEffect[i].size.x,
+					healParticleEffect[i].position.y + healParticleEffect[i].size.y,
+
+					healParticleEffect[i].position.x - healParticleEffect[i].size.x,
+					healParticleEffect[i].position.y - healParticleEffect[i].size.y,
+
+					healParticleEffect[i].position.x + healParticleEffect[i].size.x,
+					healParticleEffect[i].position.y - healParticleEffect[i].size.y,
+
+					0, 0,
+					32, 32,
+
+					circleTexture,
+					0xFFFFFFFF00 + healParticleEffect[i].currentAlpha
+				);
+			}
+		}
+
+		Novice::ScreenPrintf(0, 10, "x : %4.2f y : %4.2f", healParticleEffect[0].position.x, healParticleEffect[0].position.y);
+		Novice::ScreenPrintf(0, 30, "x : %4.2f y : %4.2f", healParticleEffect[1].position.x, healParticleEffect[1].position.y);
+		Novice::ScreenPrintf(0, 50, "x : %4.2f y : %4.2f", healParticleEffect[2].position.x, healParticleEffect[2].position.y);
+		Novice::ScreenPrintf(0, 70, "x : %4.2f y : %4.2f", healParticleEffect[3].position.x, healParticleEffect[3].position.y);
 
 		/*********************************
 			描画処理ここまで

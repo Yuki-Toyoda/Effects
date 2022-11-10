1a61897865019c667d09033be0c56b8a0560e720
夕輝 豊田 <K022G0030@g.neec.ac.jp>
2022-11-10 14:08:41 +0900

    a

--- EffectTemplate/main.cpp
+++ EffectTemplate/main.cpp
@@ -52,7 +52,11 @@ struct Effect {
 	Vector2D startPosition;
 	Vector2D endPosition;
 	Vector2D size;
+	Vector2D startSize;
 	Vector2D velocity;
+	float startStrength;
+	float strength;
+	float amplitude;
 	float acceleration;
 	float theta;
 	float elapseFrame;
@@ -68,6 +72,8 @@ struct Object {
 	Vector2D position;
 	Vector2D radius;
 	Vector2D startRadius;
+	float velocity;
+	float acceleration;
 };
 
 /*********************************
@@ -79,46 +85,72 @@ struct Object {
 *********************************/
 
 /******** エフェクト更新処理 **********/
-void AnniEffectUpdate(Effect& anniEffect, Object& object) {
-
+void AnniEffectUpdate(Effect& anniEffect, Object& object, bool& next, int& effectQuantity) {
 	if (anniEffect.init == true) {
 
 		//位置等を初期化
-		anniEffect.position = { My::RandomF(object.position.x - object.radius.x / 2, object.position.x + object.radius.x / 2, 1), My::RandomF(object.position.y - object.radius.y, object.position. y+ object.radius.y, 1) };
+		anniEffect.position = { My::RandomF(object.position.x - object.radius.x / 2, object.position.x + object.radius.x / 2, 1), My::RandomF(object.position.y - object.radius.y / 2, object.position.y+ object.radius.y / 2, 1) };
 		anniEffect.startPosition = { anniEffect.position.x, anniEffect.position.y };
-		anniEffect.endPosition = { anniEffect.startPosition.x, anniEffect.startPosition.y - 100 };
-		anniEffect.velocity = { My::RandomF(5.0f, 7.0f, 1), My::RandomF(5.0f, 7.0f, 1) };
-		anniEffect.size = { 5, 5 };
+		anniEffect.endPosition = { anniEffect.startPosition.x, anniEffect.position.y - My::RandomF(200.0f, 300.0f, 1) };
+
+		anniEffect.size = { My::RandomF(5.0f, 7.5f, 0), anniEffect.size.x };
+		anniEffect.startSize = { anniEffect.size.x, anniEffect.size.x };
+
+		anniEffect.strength = My::RandomF(60.0f, 100.0f, 0);
+		anniEffect.startStrength = anniEffect.strength;
+		anniEffect.amplitude = 0.5f;
 
 		anniEffect.time = 0.0f;
 
-		//エフェクトが向かう方向をランダムにする
-		anniEffect.theta = My::Random(0, 180);
-		anniEffect.theta = anniEffect.theta * (M_PI / 180.0f);
+		next = false;
 
 		//エフェクト表示
 		anniEffect.isEnd = false;
+
+		effectQuantity += 1;
 
 		//初期化フラグfalse
 		anniEffect.init = false;
 
 	}
 
-	if (anniEffect.elapseFrame >= 100) {
+	if (anniEffect.size.x <= 0.0f) {
 
 		//エフェクト消去
 		anniEffect.isEnd = true;
-
-		object.radius = {0.0f, 0.0f};
 
 		//経過フレーム初期化
 		anniEffect.elapseFrame = 0.0f;
 
 	}
 
+	if (anniEffect.elapseFrame == 1.0f && object.radius.x > 0.0f) {
+		next = true;
+	}
+	else {
+		effectQuantity = 0;
+	}
+
 	if (anniEffect.isEnd == false) {
 
+		//粒子エフェクトのイージング処理
 		anniEffect.time += 0.01f;
+		anniEffect.easeTime = 1.0f - powf(1.0f - anniEffect.time, 3.0f);
+
+		//粒子エフェクトのサイズ変更
+		anniEffect.size.x = (1.0 - anniEffect.easeTime) * anniEffect.startSize.x + anniEffect.easeTime * 0;
+		anniEffect.size.y = (1.0 - anniEffect.easeTime) * anniEffect.startSize.y + anniEffect.easeTime * 0;
+
+		anniEffect.theta += M_PI / anniEffect.strength;
+		anniEffect.position.x += sinf(anniEffect.theta);
+
+		anniEffect.strength = (1.0 - anniEffect.easeTime) * anniEffect.startStrength + anniEffect.easeTime * 30;
+
+		//粒子エフェクトを徐々に上に
+		anniEffect.easeTime = anniEffect.time * anniEffect.time;
+
+		anniEffect.currentAlpha = (1.0 - anniEffect.easeTime) * 0xFF + anniEffect.easeTime * 0x00;
+		anniEffect.position.y = (1.0 - anniEffect.easeTime) * anniEffect.startPosition.y + anniEffect.easeTime * anniEffect.endPosition.y;
 
 		//経過フレーム加算
 		anniEffect.elapseFrame += 1.0f;
@@ -151,7 +183,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	/******** エフェクト関係 **********/
 	//表示可能エフェクト数
-	const int maxEffects = 30;
+	const int maxEffects = 50;
+
+	int effectQuantity = 0;
+	bool next = false;
+
+	int mousePosX = 0;
+	int mousePosY = 0;
 
 	//エフェクト
 	Effect anniEffect[maxEffects];
@@ -162,6 +200,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			{0.0f, 0.0f},
 			{0.0f, 0.0f},
 			{1.0f, 1.0f},
+			{1.0f, 1.0f},
+			0.0f,
+			0.0f,
+			0.0f,
 			0.0f,
 			0.0f,
 			0.0f,
@@ -195,16 +237,43 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			更新処理ここから
 		*********************************/
 
-		if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+		if (Novice::IsTriggerMouse(0)) {
+
+			Novice::GetMousePosition(&mousePosX, &mousePosY);
+
+			object.position = { (float)mousePosX, (float)mousePosY };
+
+			object.radius = { 300.0f, 20.0f };
+			object.velocity = 1.0f;
+			object.acceleration = 0.05f;
+			next = true;
+
+		}
+
+
+		if (next == true) {
 			for (int i = 0; i < maxEffects; i++) {
-				object.radius = {100.0f, 20.0f};
-				anniEffect[i].init = true;
+				if (next == true && anniEffect[i].isEnd == true) {
+					anniEffect[i].init = true;
+					next = false;
+				}
 			}
 		}
 
-		for (int i = 0; i < maxEffects; i++) {
-			AnniEffectUpdate(anniEffect[i], object);
+		if (object.radius.x > 0) {
+			object.radius.x -= object.velocity;
+			//object.radius.y -= object.velocity;
+			object.velocity += object.acceleration;
 		}
+		else {
+			object.radius.x = 0;
+		}
+
+		for (int i = 0; i < maxEffects; i++) {
+			AnniEffectUpdate(anniEffect[i], object, next, effectQuantity);
+		}
+
+		Novice::ScreenPrintf(0, 10, "Quantity : %d", effectQuantity);
 
 		/*********************************
 			更新処理ここまで
@@ -213,30 +282,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/*********************************
 			描画処理ここから
 		*********************************/
-		/******** エフェクト描画 **********/
-		for (int i = 0; i < maxEffects; i++) {
-			if (!anniEffect[i].isEnd) {
-				Novice::DrawQuad(
-					anniEffect[i].position.x - anniEffect[i].size.x,
-					anniEffect[i].position.y + anniEffect[i].size.y,
-
-					anniEffect[i].position.x + anniEffect[i].size.x,
-					anniEffect[i].position.y + anniEffect[i].size.y,
-
-					anniEffect[i].position.x - anniEffect[i].size.x,
-					anniEffect[i].position.y - anniEffect[i].size.y,
-
-					anniEffect[i].position.x + anniEffect[i].size.x,
-					anniEffect[i].position.y - anniEffect[i].size.y,
-
-					0, 0,
-					32, 32,
-
-					circleTexture,
-					WHITE
-				);
-			}
-		}
 
 		Novice::DrawQuad(
 			object.position.x - object.radius.x / 2,
@@ -257,6 +302,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			sampleTexture,
 			RED
 		);
+
+		/******** エフェクト描画 **********/
+		for (int i = 0; i < maxEffects; i++) {
+			if (!anniEffect[i].isEnd) {
+				Novice::DrawQuad(
+					anniEffect[i].position.x - anniEffect[i].size.x,
+					anniEffect[i].position.y + anniEffect[i].size.y,
+
+					anniEffect[i].position.x + anniEffect[i].size.x,
+					anniEffect[i].position.y + anniEffect[i].size.y,
+
+					anniEffect[i].position.x - anniEffect[i].size.x,
+					anniEffect[i].position.y - anniEffect[i].size.y,
+
+					anniEffect[i].position.x + anniEffect[i].size.x,
+					anniEffect[i].position.y - anniEffect[i].size.y,
+
+					0, 0,
+					32, 32,
+
+					circleTexture,
+					0xFFFFFFFF00 + anniEffect[i].currentAlpha
+				);
+			}
+		}
 
 		/*********************************
 			描画処理ここまで
